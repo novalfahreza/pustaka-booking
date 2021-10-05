418be4a40fd9c22c544c620f22eb8f21c577e3e9
novalfahreza <maufal.f.persib@gmail.com>
2021-10-05 11:35:15 +0700

    pustakabooking. form validation

---
+++ application/views/v_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+ <title>Web Prog II | Mengenal Lebih Jauh Covid-19</title>
+ <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+</head>
+<body>
+ <div id="wrapper">
+ <header>
+ <hgroup>
+ <h1>Covid In Indonesia</h1>
+ <h3>Cara Mencegah Covid 19 
+Yang Menyebar Di Indonesia</h3>
+ </hgroup>
+ <nav>
+ <ul>
+ <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+ <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+ </ul>
+ </nav>
+ <div class="clear"></div>
+ </header>
